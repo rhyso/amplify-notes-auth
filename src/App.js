34690101ffde1c3e4b86b495d3b23491f2713f3d
rhyso <rhyswork@gmail.com>
2022-11-15 15:09:11 +0000

    show notes

--- src/App.js
+++ src/App.js
@@ -101,7 +101,33 @@ const App = ({ signOut }) => {
         </Flex>
       </View>
       <Heading level={2}>Current Notes</Heading>
+      <View margin="3rem 0">
 
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
+        
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
