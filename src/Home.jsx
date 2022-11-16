8b51abda10d6675d53e71e5ac244cfe2f149b407
rhyso <rhyswork@gmail.com>
2022-11-16 15:25:21 +0000

    added footer and logo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { Heading, View } from '@aws-amplify/ui-react';
-import logo from './images/mb7.svg';
-import logoWhite from './images/mb7-white.svg';
+import logo from './images/mb7-just-black.svg';
+import logoWhite from './images/white.svg';
+import Footer from './footer';
+
 import './App.css';
 // eslint-disable-next-line import/no-unresolved
 import '@aws-amplify/ui-react/styles.css';
@@ -13,8 +15,8 @@ function Home() {
       <header className="hero">
         <div className="logo-wrapper">
           <section>
-            <img className="bottom" width="200" alt="dsd" src={logo} />
-            <img className="top" width="200" alt="ds" src={logoWhite} />
+            <img className="bottom" width="200" height="200" alt="dsd" src={logo} />
+            <img className="top" width="200" alt="ds" height="200" src={logoWhite} />
           </section>
         </div>
         <div className="body-content">
@@ -28,6 +30,8 @@ function Home() {
         <Heading level={1}>My Notes App</Heading>
         <strong>TESTING HOME PAGE</strong>
       </div>
+
+      <Footer />
     </View>
   );
 }
